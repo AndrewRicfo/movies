b6cafa24be8484c625d72a0bee38bbbb0e2ffa57
Andrew <bloodfurry9595@gmail.com>
2017-07-23 20:49:45 +0300

    +validation when adding movie

--- client/components/MovieEditor.jsx
+++ client/components/MovieEditor.jsx
@@ -28,14 +28,32 @@ class MovieEditor extends React.Component {
 
     handleYearChange(event) {
         this.setState({ year: event.target.value });
+        if(!/^[0 -9]{4}$/.test(event.target.value) && event.target.value.length != 0) {
+            event.currentTarget.className = 'MovieEditor__input--warn';
+        }
+        else {
+            event.currentTarget.className = 'MovieEditor__input';
+        }
     };
 
     handleFormatChange(event) {
         this.setState({ format: event.target.value });
+        if(!/^(VHS)$|^(DVD)$|^(Blu-Ray)$/i.test(event.target.value) && event.target.value.length != 0) {
+            event.currentTarget.className = 'MovieEditor__input--warn';
+        }
+        else {
+            event.currentTarget.className = 'MovieEditor__input';
+        }
     };
 
     handleStarringChange(event) {
         this.setState({ starring: event.target.value });
+        if(/[^a-z,\s]/i.test(event.target.value) && event.target.value.length != 0) {
+            event.currentTarget.className = 'MovieEditor__input--warn';
+        }
+        else {
+            event.currentTarget.className = 'MovieEditor__input';
+        }
     };
 
     handleMovieAdd() {
@@ -63,14 +81,15 @@ class MovieEditor extends React.Component {
                 <input
                     type='text'
                     className='MovieEditor__input'
-                    placeholder='Enter year'
+                    placeholder='Enter release year(4 numbers)'
                     value={this.state.year}
                     onChange={this.handleYearChange}
+                    onBlur={this.handleYearChange}
                 />
                 <input
                     type='text'
                     className='MovieEditor__input'
-                    placeholder='Enter format'
+                    placeholder='Format: VHS, DVD, Blu-Ray'
                     value={this.state.format}
                     onChange={this.handleFormatChange}
                 />
@@ -84,10 +103,10 @@ class MovieEditor extends React.Component {
                 <div className='MovieEditor__footer'>
                     <button
                         className='MovieEditor__button'
-                        disabled={!this.state.title||!this.state.year||!this.state.format||!this.state.starring}
+                        disabled={!this.state.title||!/^[0 -9]{4}$/.test(this.state.year)||!/^(VHS)$|^(DVD)$|^(Blu-Ray)$/i.test(this.state.format)||/[^a-z,\s]/i.test(this.state.starring)||!this.state.starring}
                         onClick={this.handleMovieAdd}
                     >
-                        Add
+                        Add movie
                     </button>
                 </div>
             </div>
